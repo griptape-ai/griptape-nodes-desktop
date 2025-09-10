5d045c47a0988f2954301dcb51c10116a8befeb6
dylanholmes <4370153+dylanholmes@users.noreply.github.com>
2025-09-10 09:24:51 +0000

    Fix windows uv install command

--- src/main/services/setup/install-uv.ts
+++ src/main/services/setup/install-uv.ts
@@ -1,24 +1,37 @@
-import { exec } from 'child_process';
+import { ChildProcess, exec, spawn } from 'child_process';
 import { attachOutputForwarder } from '../../utils/child-process/output-forwarder';
 import { getUvInstallDir } from '../config/paths';
 
 export async function installUv(userDataDir: string): Promise<void> {
-
-  const platform = process.platform;
-  const arch = process.arch;
-  // const uvProcess = spawn(uvExecutable, ['python', 'install', getPythonVersion()], {
-  //   env: getEnv(userDataDir),
-  // });
-  // await attachOutputForwarder(uvProcess, {
-  //   logPrefix: 'INSTALL_PYTHON'
-  // });
-
   const uvInstallDir = getUvInstallDir(userDataDir);
-
-  const command = (platform === 'win32')
-  ? `powershell -ExecutionPolicy ByPass -c {$env:UV_INSTALL_DIR = "${uvInstallDir}";irm https://astral.sh/uv/install.ps1 | iex}`
-  : `curl -LsSf https://astral.sh/uv/install.sh | env UV_UNMANAGED_INSTALL="${uvInstallDir}" UV_NO_MODIFY_PATH=1 sh`;
-
-  const child = exec(command);
+  const child = (process.platform === 'win32') ? windows(uvInstallDir) : unix(uvInstallDir);
   await attachOutputForwarder(child, { logPrefix: 'INSTALL-UV' });
+}
+
+function windows(uvInstallDir: string): ChildProcess {
+  return spawn(
+      'powershell.exe',
+      [
+        '-ExecutionPolicy', 'ByPass',
+        '-c', `$env:UV_INSTALL_DIR = "${uvInstallDir}";irm https://astral.sh/uv/install.ps1 | iex`,
+      ],
+      {
+        // Important! We must not use the default value of `process.env`. If we do,
+        // then we may indavertently inherit incorrect powershell module paths from
+        // a parent process. For example if we are in development mode and run `npm start`
+        // from powershell 7, but the default installed version of powershell.exe is 5,
+        // then we will get a bunch of errors relating to import failures of core powershell
+        // modules.
+        env: {},
+      }
+    )
+}
+
+function unix(uvInstallDir: string): ChildProcess {
+  return exec(
+    `curl -LsSf https://astral.sh/uv/install.sh | env UV_UNMANAGED_INSTALL="${uvInstallDir}" UV_NO_MODIFY_PATH=1 sh`,
+    {
+      env: {},
+    }
+  )
 }
