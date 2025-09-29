f5c3910e6884624d6d590c40742611d4cf1cf3c5
dylanholmes <4370153+dylanholmes@users.noreply.github.com>
2025-09-29 17:10:32 +0200

    Add entitlements

--- scripts/build-osx.sh
+++ scripts/build-osx.sh
@@ -45,6 +45,7 @@ if [[ -n "$GITHUB_ACTIONS" && -n "$MAC_CERTS_P12" ]]; then
     VPK_ARGS+=(--signAppIdentity "Developer ID Application")
     VPK_ARGS+=(--notaryProfile "velopack-profile")
     VPK_ARGS+=(--keychain "$RUNNER_TEMP/app-signing.keychain-db")
+    VPK_ARGS+=(--signEntitlements "entitlements.plist")
 fi
 
 vpk pack "${VPK_ARGS[@]}"
