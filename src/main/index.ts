9e74ae93301d540950e6c2007d036868ec687ba3
Collin Dutter <collin@griptape.ai>
2025-10-20 16:13:26 -0700

    fix: improve engine shutdown (#117)

--- src/main/index.ts
+++ src/main/index.ts
@@ -143,7 +143,10 @@ const createWindow = () => {
 
   // Stop engine when window is closed
   mainWindow.on('closed', () => {
-    engineService.stopEngine()
+    // Ensure the async stopEngine completes properly
+    ;(async () => {
+      await engineService.stopEngine()
+    })()
   })
 }
 
